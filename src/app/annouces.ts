1a4e4d6c39be185baf7f9371e67e203e038c4765
ahmedcheikh20 <ahmedc392@gmail.com>
2021-10-21 11:29:10 +0100

    render data

--- src/app/annouces.ts
+++ src/app/annouces.ts
@@ -1,15 +1,41 @@
 export interface Announce{
     username : string;
-    title : string,
-    street : string,
-    city: string,
+    adress: string;
+    category: string,
     phoneNumber : number,
     price: number,
-    imaageUrl : string 
+    imageUrl : string 
 }
 
-export const ANNONCES : Announce = [
+export const ANNONCES : Announce[] = [
     { 
-        
+        username: 'Wissem',
+        category : 'phone',
+        adress: 'mourouj',
+        phoneNumber : 22151525,
+        price:  775 ,
+        imageUrl : 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'
+    },
+    { 
+        username: 'Ahmed',
+        category : 'phone',
+        adress: 'mourouj',
+        phoneNumber : 25611525,
+        price:  854 ,
+        imageUrl : 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'
+    },    { 
+        username: 'heni',
+        category : 'house',
+        adress: 'jerba',
+        phoneNumber : 22151525,
+        price:  968 ,
+        imageUrl : 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'
+    },    { 
+        username: 'Wissem',
+        category : 'phone',
+        adress: 'mourouj',
+        phoneNumber : 22151525,
+        price:  775 ,
+        imageUrl : 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'
     }
 ]
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ANNONCES } from '../annouces';
+
 
 @Component({
   selector: 'app-cars',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsComponent implements OnInit {
 
+  cars = ANNONCES
   constructor() { }
 
   ngOnInit(): void {
