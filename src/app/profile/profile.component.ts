df77b85ae1058793986c2f9e9df96dff7fa73024
WissemBrinis <wissembenbrinis@gmail.com>
2021-10-23 16:34:46 +0100

    session added success

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Indata } from '../check';
+import { Component, OnInit } from "@angular/core";
+import { Indata } from "../check";
 import { HttpClient } from "@angular/common/http";
-
-
+import { TokenStorageService } from "../_services/token-storage.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent implements OnInit {
+  currentUser: any;
+  constructor(private http: HttpClient, private token: TokenStorageService) {}
 
-   constructor(private http:HttpClient){}
+  data = new Indata("", "", "", 0, 0, "", "");
 
- 
-  data = new Indata('','','',0,0,'','');
-  
-
-  onSubmit() { 
-    this.http.post('server',this.data) }
-
-  ngOnInit(): void {
+  onSubmit() {
+    this.http.post("server", this.data);
   }
 
+  ngOnInit(): void {
+    this.currentUser = this.token.getUser();
+  }
 }
