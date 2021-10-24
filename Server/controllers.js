4b0f668707654468c4bd4153e0a33562f110c3ac
heniLtaief <heniltaief@gmail.com>
2021-10-24 14:49:59 +0100

    fix car announces

--- Server/controllers.js
+++ Server/controllers.js
@@ -11,8 +11,8 @@ const { constructorParametersDownlevelTransform } = require("@angular/compiler-c
 // const path = require('path')
 
 exports.createAnnounce = (req, res) => {
-  var username = req.body.productInfo + 'foo'
-  var article = {...req.body,username}
+  var productInfo = req.body.productInfo + 'foo'
+  var article = {...req.body,productInfo}
   console.log(article)
   Announce.create(article)
     .then((result) => res.send(result))
